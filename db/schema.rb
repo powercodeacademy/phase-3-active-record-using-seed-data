200c8b4e311b3bced137b6f3bd5fcbb842b74684
Mike <mclancy96@gmail.com>
2025-07-25 11:04:52 -0400

    Fix gem versions and needs_migration? method

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_134231) do
-
+ActiveRecord::Schema[8.0].define(version: 2021_07_18_134231) do
   create_table "games", force: :cascade do |t|
     t.string "title"
     t.string "genre"
     t.string "platform"
     t.integer "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
-
 end
